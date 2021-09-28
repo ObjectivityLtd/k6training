4d39539e954857d809ce7864856c3ed7f09c1ccb
Gabriel Starczewski <starczewski@objectivity.co.uk>
2021-09-28 09:05:33 +0200

    answer

--- 005_checks/02_checks.js
+++ 005_checks/02_checks.js
@@ -1,15 +1,16 @@
 import http from 'k6/http';
-import {check} from 'k6';
+import { check } from 'k6';
 
 const URL = "https://appxx.azurewebsites.net/"
 
 export default function () {
     let res = http.request('GET', URL);
     let checkResult = check(res, {
-        'response code was 200': (res) => res.status == 200,
-        'body size was 1234 bytes': (res) => res.body.length < 1234,
-        'dummy check' : ()=> 'a' === 'a'
+        'response code was 200': (res) => res.status === 200,
+        'remote port was 443': (res) => res.remote_port === 443,
+        'dummy check': () => 'a' === 'a'
     });
+    console.log(JSON.stringify(res, null,2));
     console.log("Check Result " + checkResult)
 }
 
